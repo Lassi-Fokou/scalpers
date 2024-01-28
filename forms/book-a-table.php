bb93bd1b6124d7c18e0693d7ef86c670609c0a33
Fokou <111089949+Lassi-Fokou@users.noreply.github.com>
2024-01-28 23:50:05 +0100

    book-a-table.php

--- forms/book-a-table.php
+++ forms/book-a-table.php
@@ -1,45 +1,27 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $message = $_POST['message'];
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // Customize the email content
+  $to = "scalpersfx123@gmail.com"; // Replace with your email address
+  $subject = "Booking Request";
+  $email_content = "Name: $name\n";
+  $email_content .= "Email: $email\n";
+  $email_content .= "Phone: $phone\n";
+  $email_content .= "Message:\n$message";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  $headers = "From: $email \r\n";
+  $headers .= "Reply-To: $email \r\n";
 
-  $book_a_table = new PHP_Email_Form;
-  $book_a_table->ajax = true;
-  
-  $book_a_table->to = $receiving_email_address;
-  $book_a_table->from_name = $_POST['name'];
-  $book_a_table->from_email = $_POST['email'];
-  $book_a_table->subject = "New table booking request from the website";
+  mail($to, $subject, $email_content, $headers);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $book_a_table->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $book_a_table->add_message( $_POST['name'], 'Name');
-  $book_a_table->add_message( $_POST['email'], 'Email');
-  $book_a_table->add_message( $_POST['phone'], 'Phone', 4);
-  $book_a_table->add_message( $_POST['date'], 'Date', 4);
-  $book_a_table->add_message( $_POST['time'], 'Time', 4);
-  $book_a_table->add_message( $_POST['people'], '# of people', 1);
-  $book_a_table->add_message( $_POST['message'], 'Message');
-
-  echo $book_a_table->send();
+  http_response_code(200);
+  echo "success";
+} else {
+  http_response_code(403);
+  echo "Error: Access Denied.";
+}
 ?>
